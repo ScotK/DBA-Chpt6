c71c400f5614658e68efb0b20b5b7b990a6bd484
Scot Kryzenske <scotkryzenske@students.abtech.edu>
2014-10-06 19:35:52 -0400

    Chapter 6 Code Assignment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studentbook::Application.config.secret_token = '04f976e682ccdb4b7f7b64991fdfdc47508913f1ccc22f785df6bf37ea8d31a9787e40c758cdf7548de126014dbc65e1491101d9ddc6b499f365c2d695197b83'
